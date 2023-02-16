6de48d0e562febfb92c255fc6692aa55de42ca9e
devhoussam1998 <devhoussam1998@gmail.com>
2023-02-16 04:42:01 -0800

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,7 @@
-/*
- * This program outputs a quote about programming to the console.
- * The quote says "Programming is like building a multilingual puzzle".
- * 
- * The main function uses the puts function to output the quote to the console.
- * The puts function automatically adds a newline character at the end of the string,
- * so there's no need to manually add one.
- * 
- * Finally, the main function returns 0, which is a conventional value to indicate
- * that the program has executed successfully.
- */
 #include <stdio.h>
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	/* Return 0 to indicate successful execution */
 	return (0);
 }
